69f287bd53b0ec2b5408f94960d8f0131a95553c
Evelin <evelindacanovas@gmail.com>
2021-06-08 20:06:42 +0200

    All project

--- src/test/java/com/cybertek/tests/day_7_testNG/BeforeAfterMethod.java
+++ src/test/java/com/cybertek/tests/day_7_testNG/BeforeAfterMethod.java
@@ -1,24 +1,39 @@
 package com.cybertek.tests.day_7_testNG;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BeforeAfterMethod {
 
     @Test
     public void test1(){
-        System.out.println("WebDriver, Opening Browser");
         System.out.println("First Test Case");
-        System.out.println("Closing Browser,Quit");
+    }
+    @Ignore
+    @Test
+    public void test2(){
+        System.out.println("Second Test Case");
+    }
+    @Test
+    public void test3(){
+        System.out.println("Third Test Case");
     }
     @BeforeMethod
     public void setUp(){
-        System.out.println("WebDriver, Opening Browser");
+        System.out.println("--WebDriver, Opening Browser--");
     }
     @AfterMethod
     public void tearDown(){
-        System.out.println("Closing Browser,Quit");
+        System.out.println("--Closing Browser,Quit--");
+    }
+    @BeforeClass
+    public void setUpClass() {
+        System.out.println("**BEFORE CLASS**");
+        System.out.println("Executed before class");
+    }
+    @AfterClass
+    public void tearDownClass(){
+        System.out.println("**AFTER CLASS**");
+        System.out.println("Executed after class");
     }
 
 
