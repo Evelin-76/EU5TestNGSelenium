f0954f5a12d130e171464cff63af607c177c0a65
Evelin <evelindacanovas@gmail.com>
2021-06-16 13:53:26 +0200

    verify is selected practices radio buttons

---
+++ src/test/java/com/cybertek/tests/day7_TipesOfElements/RadioButtonTest.java
@@ -0,0 +1,41 @@
+package com.cybertek.tests.day7_TipesOfElements;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RadioButtonTest {
+
+    @Test
+    public void test1(){
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+
+        //locating radio button "blue" and "red"
+        WebElement blueRadioButton = driver.findElement(By.id("blue"));
+        WebElement redRadioButton = driver.findElement(By.id("red"));
+
+        //check this condition printing it.
+      //  System.out.println("blueRadioButton.isSelected() = " + blueRadioButton.isSelected());
+      //  System.out.println("redRadioButton.isSelected() = " + redRadioButton.isSelected());
+
+        //how to click radio buttons:
+        redRadioButton.click();
+
+        //verify blue is selected and red is not selected
+        Assert.assertTrue(blueRadioButton.isSelected(), "verify that blue is selected");
+        Assert.assertFalse(redRadioButton.isSelected(), "verify that red is NOT selected");
+
+        //verify red is selected and red is not selected
+        Assert.assertTrue(redRadioButton.isSelected(), "verify that blue is selected");
+        Assert.assertFalse(blueRadioButton.isSelected(), "verify that red is NOT selected");
+
+        driver.quit();
+
+    }
+}
--- src/test/java/com/cybertek/tests/day_7_testNG/TestNGAssertionDemo.java
+++ src/test/java/com/cybertek/tests/day_7_testNG/TestNGAssertionDemo.java
@@ -8,6 +8,30 @@ import org.testng.annotations.Test;
 public class TestNGAssertionDemo {
 
     @Test
+    public void test3(){
+        String expectedTitle = "Cyt";
+        String actualTitle = " Cybertek";
+
+        Assert.assertTrue(actualTitle.startsWith(expectedTitle),"Verify title starts with Cyb");
+    }
+    @Test
+    public void test4(){
+        //verify email contains @ sign
+        String email = "mike@smith.com";
+
+        Assert.assertTrue(email.contains("@"), "Verify email contains @");
+    }
+    @Test
+    public void test5(){
+        Assert.assertFalse(0 < 1, "Verify that 0 is not greater than 1");
+    }
+    @Test
+    public void test6() {
+        System.out.println("Assert.assertNotEquals(\"one\",\"once\")");
+        Assert.assertNotEquals("one","once");
+    }
+
+    @Test
     public void test1(){
         System.out.println("First Assertion");
         Assert.assertEquals("title","title");
@@ -25,7 +49,7 @@ public class TestNGAssertionDemo {
     }
     @AfterMethod
     public void tearD(){
-        System.out.println("Closw browser");
+        System.out.println("Close browser");
     }
 
 }
