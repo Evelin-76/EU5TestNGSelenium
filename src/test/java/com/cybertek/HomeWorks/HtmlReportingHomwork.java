97b1259bd040e79d4b481c7f6119063958f57aea
Evelin <evelindacanovas@gmail.com>
2021-07-26 15:40:52 +0200

    HtmlReportingHomwork ready to start

---
+++ src/test/java/com/cybertek/HomeWorks/HtmlReportingHomwork.java
@@ -0,0 +1,14 @@
+package com.cybertek.HomeWorks;
+
+public class HtmlReportingHomwork {
+    /*
+    open the chrome
+    go to qa1.vytrack
+    login as a sales manager
+    navigate to customers -> contacts
+    click on email mbrackstone9@example.com
+    verify that full name is Mariam Brackstone
+    verify that email is mbrackstone9@example.com
+    verify that phone number is +198982323434
+     */
+}
