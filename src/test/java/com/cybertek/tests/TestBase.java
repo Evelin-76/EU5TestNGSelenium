bad52dc5e31c099b95b65ea19a652d9be0fcc67f
Evelin <evelindacanovas@gmail.com>
2021-07-26 12:50:27 +0200

    Adding ExtentReport to TestBase

--- src/test/java/com/cybertek/tests/TestBase.java
+++ src/test/java/com/cybertek/tests/TestBase.java
@@ -1,13 +1,14 @@
 package com.cybertek.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,6 +16,47 @@ public class TestBase {
     protected WebDriver driver;
     protected Actions actions;
     protected WebDriverWait wait;
+    //this class is for used for starting and building reports
+    protected ExtentReports report;
+    //this class is used for create HTML file
+    protected ExtentHtmlReporter htmlReporter;
+    //this will defint a test, enables adding logs, authors test steps
+    protected ExtentTest extentLogger;
+
+    @BeforeTest
+    public void setUpTest(){
+        //initialize the class
+        report = new ExtentReports();
+
+        //create a report path:
+        //1. this is the project path where our project is locator:
+        String projectPath = System.getProperty("user.dir");
+        //this is my file path for report, it is where my project path is dynamic
+        String path = projectPath + "/test-output/report.html";
+
+        //initialize the html reporter with the report path
+        htmlReporter = new ExtentHtmlReporter(path);
+
+        //attach the html report to report object and create the connection between them
+        report.attachReporter(htmlReporter);
+
+        //title in report
+        htmlReporter.config().setReportName("Vytrack Smoke Test");
+
+        //set environment information
+        report.setSystemInfo("Environment", "QA");
+        report.setSystemInfo("Browser", ConfigurationReader.get("browser"));
+        report.setSystemInfo("OS", System.getProperty("os.name"));
+
+    }
+    @AfterTest
+    public void tearDownTest(){
+        //this is when the report is actually created
+        report.flush();
+    }
+
+
+
 
     @BeforeMethod
     public void setUp(){
